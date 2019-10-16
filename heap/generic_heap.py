8fb42ba3f58561b007bd9a54159c123eff8386cf
danielhorsley <danielhorsley@users.noreply.github.com>
2019-10-16 15:52:16 +0100

    heap first draft

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,56 @@
 class Heap:
-    def __init__(self, comparator):
+    def __init__(self, comparator = lambda x,y : x > y):
         self.storage = []
         self.comparator = comparator
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        for i in range(len(self.storage)):
+          self._bubble_up(i)
+
 
     def delete(self):
-        pass
+        if len(self.storage)==0:
+          return None
+        ret = self.storage[0]
+        self.storage = self.storage[1:]
+        return ret
 
     def get_priority(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        if index == 0:
+          pass
+        else:
+          parent_number = (index-1)//2
+          if self.comparator(self.storage[index],self.storage[parent_number]):
+            a = self.storage[index]
+            b = self.storage[parent_number]
+            self.storage[index] = b
+            self.storage[parent_number] = a
+          else:
+            pass
 
     def _sift_down(self, index):
-        pass
+        if len(self.storage)>index*2 + 1:
+          child_number_one = index*2 + 1
+
+          if len(self.storage)>index*2 + 2:
+            child_number_two = index*2 + 2
+            if self.comparator(self.storage[child_number_one],self.storage[child_number_two]):
+              greater_child = child_number_one
+            else:
+              greater_child = child_number_two
+          else:
+            greater_child = child_number_one
+          if self.comparator(self.storage[greater_child],self.storage[index]):
+            a = self.storage[greater_child]
+            b = self.storage[index]
+            self.storage[index] = a
+            self.storage[greater_child] = b
+        else:
+          pass
